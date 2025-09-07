34aee6b0e0b20d59dc887ff3f63e7aa20af39643
Zionncube <mercy.msawenkosi@gmail.com>
2025-09-07 20:31:21 +0200

    commit

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/contact'); // qinisekisa ukuthi i-model path ilungile
+const Contact = require('../models/contact');
 
 // GET all contacts
 router.get('/', async (req, res) => {
@@ -23,43 +23,26 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// ✅ POST - create a new contact
-router.post("/", async (req, res) => {
-    try {
-        const { firstName, lastName, email, favoriteColor, birthday } = req.body;
-
-        // create new contact
-        const newContact = new Contact({
-            firstName,
-            lastName,
-            email,
-            favoriteColor,
-            birthday
-        });
-
-        // save to DB
-        const savedContact = await newContact.save();
-        res.status(201).json(savedContact);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+// POST new contact
+router.post('/', async (req, res) => {
+  try {
+    const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+    const newContact = new Contact({ firstName, lastName, email, favoriteColor, birthday });
+    const savedContact = await newContact.save();
+    res.status(201).json(savedContact);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-// PUT (update contact)
+// PUT update contact
 router.put('/:id', async (req, res) => {
   try {
     const updatedContact = await Contact.findByIdAndUpdate(
       req.params.id,
-      {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-      },
+      req.body,
       { new: true }
     );
-
     if (!updatedContact) return res.status(404).json({ message: 'Contact not found' });
     res.status(200).json(updatedContact);
   } catch (err) {
